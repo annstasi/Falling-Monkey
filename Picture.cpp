f2d4ba3f9b748032ad31bc02844358d609de2739
annstasi <67387536+annstasi@users.noreply.github.com>
2020-06-25 19:24:17 +0300

    Delete Picture.cpp

--- Picture.cpp
+++
@@ -1,7 +0,0 @@
-#include "picture.h"
-#include <QGraphicsScene>
-#include <QList>
-picture::picture(): QObject(), QGraphicsPixmapItem() {
-    setPixmap(QPixmap(":/images/banana.png"));
-    setPos(8,45);
-}
